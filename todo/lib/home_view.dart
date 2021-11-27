ca95c0214c0db645865aa919db0e4b4050f01f62
Charles Ewudzie <cewudzie0@gmail.com>
2021-11-26 14:42:35 -1200

    New

--- todo/lib/home_view.dart
+++ todo/lib/home_view.dart
@@ -1,13 +1,22 @@
+
+
 import 'package:flutter/material.dart';
-class HomeView extends StatelessWidget {
+class HomeView extends StatelessWidget   {
   const HomeView({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      
+    return  Scaffold(   
   appBar: AppBar(
+    leading: const Icon(Icons.menu, size:30, color:Colors.white),
+
+  actions:const[
+    IconButton(onPressed: null, icon: Icon(Icons.search))
+  ],
+  // IconButton(){} null,icon:const Icon(Icons:menu),
+
+
     centerTitle:true,
     backgroundColor: Colors.indigo,
     title: const Text(
@@ -23,9 +32,18 @@ class HomeView extends StatelessWidget {
       // wordSpacing:100,
      ), 
       ),
-     
+  ),
+  body: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: const[
+      Text("Charles"),
+     CircleAvatar(radius:30,backgroundImage:AssetImage('assets/images/whatsapp.png'))
+    ],
+    
   )
  
-    );
-}
+  
+      );
+  }
+
 }
